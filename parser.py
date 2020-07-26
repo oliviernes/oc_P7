bb00c64f2d6f4fd8a8b723b5bf4336ee5b5e3910
Olivier71 <olivier.nesme@gmail.com>
2020-07-26 21:45:40 +0200

    Add a class Parser

--- parser.py
+++ parser.py
@@ -7,14 +7,19 @@ import pdb
 
 from config import EXTRA_WORDS, STOP_WORDS
 
-def parser(phrase):
-    """"Parse a phrase"""
-    ERASE_WORDS = EXTRA_WORDS + STOP_WORDS
-    
-    phrase_no_accents = unidecode.unidecode(phrase)
-    phrase_lower = phrase_no_accents.lower()
-    phrase_alphanum = re.sub('[^A-Za-z0-9-]+', ' ', phrase_lower)
-    phrase_sliced = phrase_alphanum.split()
-    words = [word for word in phrase_sliced if word not in ERASE_WORDS]
-    search_words = " ".join(words)
-    return search_words
+class Parser:
+    """Class which allow to parse sentences"""
+
+    def __init__(self):
+        self.erase_words = EXTRA_WORDS + STOP_WORDS
+
+    def parse(self, phrase):
+        """Parse a phrase"""
+
+        phrase_no_accents = unidecode.unidecode(phrase)
+        phrase_lower = phrase_no_accents.lower()
+        phrase_alphanum = re.sub('[^A-Za-z0-9-]+', ' ', phrase_lower)
+        phrase_sliced = phrase_alphanum.split()
+        words = [word for word in phrase_sliced if word not in self.erase_words]
+        search_words = " ".join(words)
+        return search_words
--- tests_grandpy_bot.py
+++ tests_grandpy_bot.py
@@ -1,14 +1,14 @@
 import pytest
-from parser import parser
+from parser import Parser
 
 def test_parser_phrase():
     phrase = "Salut Grandpy, raconte moi à propos d'Openclassrooms."
-    phrase_parsed = parser(phrase)
-    
-    assert phrase_parsed == "openclassrooms"
+    phrase_parsed = Parser()
+
+    assert phrase_parsed.parse(phrase) == "openclassrooms"
 
 def test_parser_adresse():
     adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos du 7 Cité Paradis, 75010 Paris"
-    phrase_parsed = parser(adresse)
+    phrase_parsed = Parser()
 
-    assert phrase_parsed == "7 cite paradis 75010 paris"
+    assert phrase_parsed.parse(adresse) == "7 cite paradis 75010 paris"
