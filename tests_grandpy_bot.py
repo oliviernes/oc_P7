ced84e05f1d46ed13c9d34ad5ed6f0d31c8da964
Olivier71 <olivier.nesme@gmail.com>
2020-07-26 20:44:58 +0200

    Add test to extract an adresse from a phrase

--- tests_grandpy_bot.py
+++ tests_grandpy_bot.py
@@ -1,8 +1,14 @@
 import pytest
 from parser import parser
 
-def test_parser():
+def test_parser_phrase():
     phrase = "Salut Grandpy, raconte moi à propos d'Openclassrooms."
     phrase_parsed = parser(phrase)
     
     assert phrase_parsed == "openclassrooms"
+
+def test_parser_adresse():
+    adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos du 7 Cité Paradis, 75010 Paris"
+    phrase_parsed = parser(adresse)
+
+    assert phrase_parsed == "7 cite paradis 75010 paris"
