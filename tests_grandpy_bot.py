d930b4e4b8c9ae24235b853bc816e63afeed8647
Olivier71 <olivier.nesme@gmail.com>
2020-07-30 10:21:51 +0200

    Delete duplicate test

--- tests_grandpy_bot.py
+++ tests_grandpy_bot.py
@@ -1,14 +1,9 @@
 import pytest
 from parser import Parser
 
-def test_parser_phrase():
-    phrase = "Salut Grandpy, raconte moi à propos d'Openclassrooms."
-    phrase_parsed = Parser()
-
-    assert phrase_parsed.parse(phrase) == "openclassrooms"
-
 def test_parser_adresse():
     adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos du 7 Cité Paradis, 75010 Paris"
     phrase_parsed = Parser()
 
     assert phrase_parsed.parse(adresse) == "7 cite paradis 75010 paris"
+
