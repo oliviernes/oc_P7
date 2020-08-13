bbf01ddb8d7e0d6ad6ef867e6507c65bccf39b31
Olivier71 <olivier.nesme@gmail.com>
2020-08-13 15:35:21 +0200

    Add class TestAPI

--- tests_grandpy_bot.py
+++ tests_grandpy_bot.py
@@ -1,5 +1,7 @@
+import json
 import pytest
-from parser import Parser
+from flaskapp.backend.parser import Parser
+from flaskapp.backend.API import Google
 
 def test_parser_adresse():
     adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos du 7 Cité Paradis, 75010 Paris"
@@ -18,3 +20,24 @@ def test_parser_STOP_WORDS_only():
     phrase_parsed = Parser()
 
     assert phrase_parsed.parse(adresse) == ""
+
+class TestAPI:
+    """Test the API response management."""
+
+    def test_geoloc_return(self, mocker):
+        result = {
+            "results" :[
+                    {
+                        'place_id' : "ChIJxfB-lhRu5kcRfK7MP5oTfH8",
+                        'geometry' : {
+                                        'location' : {'lat': 48.8747265, 'lng': 2.3505517}
+                                    }
+                    }
+                ],
+        }
+
+        mocker.patch('flaskapp.backend.API.Get_json.get_json', return_value = result)
+
+        goggle = Google()
+
+        assert goggle.geoloc('openclassrooms') == { 'locate': {'lat': 48.8747265, 'lng': 2.3505517}, 'place_id': "ChIJxfB-lhRu5kcRfK7MP5oTfH8" }
