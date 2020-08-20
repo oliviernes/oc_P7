f0e9320c6973b040116fc0383a754fc678fe3397
Olivier71 <olivier.nesme@gmail.com>
2020-08-20 14:36:08 +0200

    Add dictConfig(logging_config) in grandpy_bot.py

--- grandpy_bot.py
+++ grandpy_bot.py
@@ -1,4 +1,9 @@
 from flaskapp import app
 
+from logging.config import dictConfig
+from flaskapp.backend.logs.logging_config import logging_config
+
+dictConfig(logging_config)
+
 # if __name__ == '__main__':
 #     app.run(debug=True)
