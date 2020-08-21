1e26337b4e8f54fb5bd96c5768cd28e0acd6bfce
Olivier71 <olivier.nesme@gmail.com>
2020-08-21 18:43:16 +0200

    Add messages to /ajax/ route

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -6,6 +6,7 @@ from flaskapp import app
 from flaskapp.forms import PapyForm
 from config import MAPBOX_API
 from flaskapp.backend.API import Google, WikiMedia
+from flaskapp.backend.messages import Message
 
 import pdb
 
@@ -13,26 +14,44 @@ import pdb
 @app.route("/ajax/", methods=["GET", "POST"])
 def ajax():
 
-    locate = ""
-
     google = Google()
     wiki = WikiMedia()
+    message = Message()
+    messages = []
 
     if request.form["Question"]:
         question = request.form["Question"]
         locate = google.geoloc(question)
-        infos_wiki = wiki.get_infos(locate["district"])
 
-    # breakpoint()
-
-    return jsonify(
-        {
-            "locate": locate["locate"],
-            "address": locate["address"],
-            "summary": infos_wiki["summary"],
-            "url": infos_wiki["url"],
-        }
-    )
+        if google.loc_data['status']:
+            infos_wiki = wiki.get_infos(locate["district"])
+            messages.append(message.positive_address())
+            if wiki.wiki_data['status']:
+                messages.append(message.positive_wiki())
+                return jsonify(
+                    {
+                        "locate": locate["locate"],
+                        "address": locate["address"],
+                        "summary": infos_wiki["summary"],
+                        "url": infos_wiki["url"],
+                        "messages": messages,
+                    }
+                )
+            else:
+                messages.append(message.negative_wiki())
+                return jsonify(
+                    {
+                        "locate": locate["locate"],
+                        "address": locate["address"],
+                        "messages": messages,
+                    }
+                )
+        else:
+            messages.append(message.negative_addresse())
+            return jsonify({ "messages": messages})
+    else:
+        messages.append("Mais pose donc une question!!")
+        return jsonify({ "messages": messages})
 
 
 @app.route("/")
