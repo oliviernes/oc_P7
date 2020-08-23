16ce3d276b4593e3bded4e57df5978736d2f3f22
Olivier71 <olivier.nesme@gmail.com>
2020-08-23 11:16:59 +0200

    Add map and spinner in ajax.js

--- flaskapp/static/js/ajax.js
+++ flaskapp/static/js/ajax.js
@@ -3,17 +3,53 @@ const spinner = document.getElementById('spin')
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    spinner.style.display == 'block';
+    spinner.style.visibility="visible";
     fetch("/ajax/", {
         method: "POST",
         body: new FormData(form)
     })
     .then(response => response.json())
     .then(response => {
-        alert(response['locate'].lat);
+        // alert(response['locate'].lat);
+        const papybotElt = document.getElementById('papybot');
+        papybotElt.innerHTML = `<div class="offset-lg-2 col-lg-10"><h1>${response['messages'][0]} ${response['address']}</h1></div>` 
+        const mapbotElt = document.getElementById('mapbot');
+        mapbotElt.innerHTML = `<div id='map' class="offset-lg-2 col-lg-10" style='width: 1000px; height: 300px;'></div>`;            
+        const wikibotElt = document.getElementById('wikibot');
+        wikibotElt.innerHTML = `<div class="offset-lg-2 col-lg-10"><p>${response['messages'][1]} ${response['summary']}</p></div>` 
+        let lati = response['locate'].lat;
+        let long = response['locate'].lng;
+        var map = new mapboxgl.Map({
+            container: 'map',
+            center: [ long, lati ],
+            style: 'mapbox://styles/mapbox/streets-v11',
+            zoom: 15,
+        }); 
     })
-    console.log("Ajax fonctionne");
+    // .then(spinner => {
+    //     spinner.style.visibility="hidden"
+    // })
+    .then(display)
 });
+
+function display(){
+    spinner.style.visibility="hidden"
+    console.log("Ajax fonctionne");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // let map = document.createElement("div")
 
