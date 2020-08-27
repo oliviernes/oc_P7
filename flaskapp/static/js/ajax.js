1dbdcb8e2d1a7ebb3ef2b7cc7f0feea72656b2e4
Olivier71 <olivier.nesme@gmail.com>
2020-08-27 10:33:10 +0200

    Add and pluralize 'recherche précédente' to the front

--- flaskapp/static/js/ajax.js
+++ flaskapp/static/js/ajax.js
@@ -1,5 +1,8 @@
 const form = document.getElementById('formi');
 const spinner = document.getElementById('spin')
+let count = 0;
+let lastquestion = ""
+let div = document.createElement('div');
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
@@ -10,9 +13,31 @@ form.addEventListener('submit', function(event){
     })
     .then(response => response.json())
     .then(response => {
-        let questionElt = document.createElement('div');
-        questionElt.innerHTML = `<div class="offset-lg-2 col-lg-10"><h1>${ response['question'] }</h1></div>` 
-        document.getElementById('questionbot').appendChild(questionElt)
+        let questionlastElt = document.createElement('div');
+        
+        if ( count == 1 ) {
+            let previous1 = `<div class="col-lg-8 box"><p id="recherches"> Recherche précédente: </p></div>`
+            let div1 = document.querySelector('#question-bar');
+            div.innerHTML = previous1;
+            div1.parentNode.insertBefore(div.firstChild, div1.nextSibling);
+        }
+        else if ( count == 2 ) {
+            let previous2 = document.createTextNode("Recherches précédentes:");
+            let recherche = document.getElementById("recherches");
+            recherche.replaceChild(previous2, recherche.firstChild);
+        }
+
+        if ( count > 0 ) {
+            questionlastElt.innerHTML = `<div class="col-lg-8 box"><p>${ lastquestion }</p></div>` 
+            document.getElementById('question-last-bot').appendChild(questionlastElt)
+        }
+
+        let questionElt = document.getElementById('questionbot');
+        questionElt.innerHTML = `<div class="col-lg-8"><h2>${ response['question'] }</h2></div>`
+        
+        lastquestion = response['question']
+
+        count += 1;
         const mapbotElt = document.getElementById('mapbot');
         const papybotElt = document.getElementById('papybot');
         const wikibotElt = document.getElementById('wikibot');
@@ -32,7 +57,7 @@ form.addEventListener('submit', function(event){
                 .setLngLat([ long, lati ])
                 .addTo(map);
             
-            papybotElt.innerHTML = `<div class="offset-lg-2 col-lg-10"><h1>${response['messages'][0]} ${response['address']}</h1></div>`
+            papybotElt.innerHTML = `<div class="offset-lg-2 col-lg-10"><h2>${response['messages'][0]} ${response['address']}</h2></div>`
             if (response['summary']) {
                 wikibotElt.innerHTML = `<div class="offset-lg-2 col-lg-10"><p>${response['messages'][1]} ${response['summary']} [<a href="${response['url']}">En savoir plus sur Wikipedia</a>]</p></div>` 
             }
@@ -42,13 +67,10 @@ form.addEventListener('submit', function(event){
         }
         else {
             mapbotElt.innerHTML = `<div class="offset-lg-2 col-lg-10" ></div>`;
-            papybotElt.innerHTML = `<div class="offset-lg-2 col-lg-10"><h1>${response['messages']}</h1></div>`;
+            papybotElt.innerHTML = `<div class="offset-lg-2 col-lg-10"><h2>${response['messages']}</h2></div>`;
             wikibotElt.innerHTML = `<div class="offset-lg-2 col-lg-10"></div>`;
         }
     })
-    // .then(spinner => {
-    //     spinner.style.visibility="hidden"
-    // })
     .then(display)
 });
 
