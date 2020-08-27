54a88fdf7972fd3b80672365284e62af2f995262
Olivier71 <olivier.nesme@gmail.com>
2020-08-27 20:09:14 +0200

    Add Mediawiki tests and an other geoloc test

--- tests_grandpy_bot.py
+++ tests_grandpy_bot.py
@@ -1,7 +1,7 @@
 import json
 import pytest
 from flaskapp.backend.parser import Parser
-from flaskapp.backend.API import Google
+from flaskapp.backend.API import Google, WikiMedia
 
 
 def test_parser_adresse():
@@ -34,7 +34,7 @@ def test_parser_apostrophe():
 class TestAPI:
     """Test the API response management."""
 
-    def test_geoloc_return(self, mocker):
+    def test_geoloc_return_OK(self, mocker):
         results = {
             "results": [
                 {
@@ -56,5 +56,66 @@ class TestAPI:
             "district": "Cité Paradis",
         }
 
-    # def test_get_infos(self, mocker):
+    def test_geoloc_bad_result(self, mocker):
+        results = {
+            "results": [
+                {
+                    "bad result": "no valid data",
+                }
+            ],
+            "status": "OK"
+        }
+
+        mocker.patch("flaskapp.backend.API.Get_json.get_json", return_value=results)
+
+        goggle = Google()
+        goggle.geoloc("bad_answer")
+
+        assert goggle.loc_data == {
+                "status": False,
+                "error": {"KeyError": "'geometry'", "response": {
+            "results": [
+                {
+                    "bad result": "no valid data",
+                }
+            ],
+            "status": "OK"
+            },},
+        }
+
+
+
+    def test_get_infos_request(self, mocker):
+        wiki = WikiMedia()
+
+        titles = ['Cité Paradis', 'OpenClassrooms', 'Vanessa Paradis', 'Baden-Baden', 'Cité de Carcassonne', 'Saison 10 de Camping Paradis', 'Paradis (homonymie)', "Rue d'Hauteville", 'Paradis', 'Liste des voies du 10e arrondissement de Paris']
         
+        summary = "La cité Paradis est une voie publique située dans le 10e arrondissement de Paris.\n\n\n== Situation et accès ==\nLa cité Paradis est une voie publique située dans le 10e arrondissement de Paris. Elle est en forme de té, une branche débouche au 43, rue de Paradis, la deuxième au 57, rue d'Hauteville et la troisième en impasse."
+        url = 'https://fr.wikipedia.org/wiki/Cit%C3%A9_Paradis'
+
+        class Page:
+            """Class to mock Mediawiki.page response"""
+            def __init__(self):
+                self.url = url
+
+        mocker.patch("flaskapp.backend.API.MediaWiki.search", return_value=titles)
+        mocker.patch("flaskapp.backend.API.MediaWiki.page", return_value=Page())
+        mocker.patch("flaskapp.backend.API.MediaWiki.summary", return_value=summary)
+        mocker.patch("flaskapp.backend.API.MediaWiki.page.url", return_value=url)
+
+        assert wiki.get_infos("Cité Paradis") == {
+            "summary": "La cité Paradis est une voie publique située dans le 10e arrondissement de Paris.\n\n\n\nLa cité Paradis est une voie publique située dans le 10e arrondissement de Paris. Elle est en forme de té, une branche débouche au 43, rue de Paradis, la deuxième au 57, rue d'Hauteville et la troisième en impasse.",
+            "url": "https://fr.wikipedia.org/wiki/Cit%C3%A9_Paradis",
+        }
+
+    def test_get_infos_no_titles(self, mocker):
+        wiki = WikiMedia()
+
+        titles = []
+        
+        mocker.patch("flaskapp.backend.API.MediaWiki.search", return_value=titles)
+
+        assert wiki.get_infos("whatever") == {
+            "summary": "",
+            "url": "",
+        }
