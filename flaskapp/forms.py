22833a1a65b77fd2e322cdcd62b0ed586b9d8f38
Olivier71 <olivier.nesme@gmail.com>
2020-08-28 16:09:34 +0200

    remove useless forms.py file

--- flaskapp/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class PapyForm(FlaskForm):
-    question = StringField("Pose une question à ton papi", validators=[DataRequired()])
-    submit = SubmitField("Envoyer")
