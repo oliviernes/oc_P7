41ac48096105eec738c97ddc7af92f4bffc9d2eb
Olivier71 <olivier.nesme@gmail.com>
2020-08-30 10:35:32 +0200

    Remove useless app.run
    Because of the use of FLASK_APP=grandpy_bot.py in .flaskenv with python-dotenv

--- grandpy_bot.py
+++ grandpy_bot.py
@@ -4,6 +4,3 @@ from logging.config import dictConfig
 from flaskapp.backend.logging.logging_config import logging_config
 
 dictConfig(logging_config)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
