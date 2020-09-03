bee058074d4dc48aa17cca909f76b83569341ce2
Olivier71 <olivier.nesme@gmail.com>
2020-09-03 14:53:22 +0200

    Add Test_messages class

--- tests_grandpy_bot.py
+++ tests_grandpy_bot.py
@@ -158,3 +158,32 @@ class TestAPI:
             "summary": "",
             "url": "",
         }
+
+class Test_messages:
+
+    def test_positive_adresse(self, mocker):
+        
+        message = Message()
+
+        mocker.patch(
+                        "flaskapp.backend.messages.rand", 
+                        return_value = "Mais t'ai-je déjà raconté" \
+                        " l'histoire de ce quartier qui m'a vu en " \
+                        "culottes courtes ?"
+                    )
+        
+        assert message.positive_address() == "Mais t'ai-je déjà raconté " \
+        "l'histoire de ce quartier qui m'a vu en culottes courtes ?"
+
+    def test_negative_adresse(self, mocker):
+        
+        message = Message()
+
+        mocker.patch(
+                        "flaskapp.backend.messages.rand", 
+                        return_value = "Je ne comprend pas ta question. " \
+                        "Parle moi mieux que ça!"
+                    )
+        
+        assert message.negative_address() == "Je ne comprend pas ta " \
+        "question. Parle moi mieux que ça!"
