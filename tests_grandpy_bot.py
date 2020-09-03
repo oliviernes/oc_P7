8f82e1348a08744fe15ff33bfbd6aea300f660a6
Olivier71 <olivier.nesme@gmail.com>
2020-09-03 15:02:00 +0200

    Format line length < 79
    To comply with PEP8

--- tests_grandpy_bot.py
+++ tests_grandpy_bot.py
@@ -3,10 +3,11 @@ import json
 import pytest
 from flaskapp.backend.parser import Parser
 from flaskapp.backend.API import Google, WikiMedia
-
+from flaskapp.backend.messages import Message
 
 def test_parser_adresse():
-    adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos du 7 Cité Paradis, 75010 Paris"
+    adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos du 7 Cité" \
+        " Paradis, 75010 Paris"
     phrase_parsed = Parser()
 
     assert phrase_parsed.parse(adresse) == "7 cite paradis 75010 paris"
@@ -27,7 +28,8 @@ def test_parser_STOP_WORDS_only():
 
 
 def test_parser_apostrophe():
-    adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos d'Openclassrooms"
+    adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos" \
+        " d'Openclassrooms"
     phrase_parsed = Parser()
 
     assert phrase_parsed.parse(adresse) == "openclassrooms"
@@ -40,8 +42,14 @@ class TestAPI:
         results = {
             "results": [
                 {
-                    "geometry": {"location": {"lat": 48.8747265, "lng": 2.3505517}},
-                    "formatted_address": "7 Cité Paradis, 75010 Paris, France",
+                    "geometry": {
+                                    "location": {
+                                                    "lat": 48.8747265,
+                                                     "lng": 2.3505517
+                                                }
+                                },
+                    "formatted_address": "7 Cité Paradis, 75010 Paris," \
+                        " France",
                     "address_components": [
                         {
                             "long_name": "7",
@@ -61,12 +69,18 @@ class TestAPI:
                         {
                             "long_name": "Arrondissement de Paris",
                             "short_name": "Arrondissement de Paris",
-                            "types": ["administrative_area_level_2", "political"],
+                            "types": [
+                                        "administrative_area_level_2", 
+                                        "political"
+                                    ],
                         },
                         {
                             "long_name": "Île-de-France",
                             "short_name": "IDF",
-                            "types": ["administrative_area_level_1", "political"],
+                            "types": [
+                                        "administrative_area_level_1", 
+                                        "political"
+                                    ],
                         },
                         {
                             "long_name": "France",
@@ -84,7 +98,10 @@ class TestAPI:
             "status": "OK",
         }
 
-        mocker.patch("flaskapp.backend.API.GetJson.get_json", return_value=results)
+        mocker.patch(
+                        "flaskapp.backend.API.GetJson.get_json", 
+                        return_value=results
+                    )
 
         goggle = Google()
 
@@ -95,9 +112,15 @@ class TestAPI:
         }
 
     def test_geoloc_bad_result(self, mocker):
-        results = {"results": [{"bad result": "no valid data",}], "status": "OK"}
+        results = {
+                    "results": [{"bad result": "no valid data",}], 
+                    "status": "OK"
+                    }
 
-        mocker.patch("flaskapp.backend.API.GetJson.get_json", return_value=results)
+        mocker.patch(
+                        "flaskapp.backend.API.GetJson.get_json", 
+                        return_value=results
+                    )
 
         goggle = Google()
         goggle.geoloc("bad_answer")
@@ -129,7 +152,12 @@ class TestAPI:
             "Liste des voies du 10e arrondissement de Paris",
         ]
 
-        summary = "La cité Paradis est une voie publique située dans le 10e arrondissement de Paris.\n\n\n== Situation et accès ==\nLa cité Paradis est une voie publique située dans le 10e arrondissement de Paris. Elle est en forme de té, une branche débouche au 43, rue de Paradis, la deuxième au 57, rue d'Hauteville et la troisième en impasse."
+        summary = "La cité Paradis est une voie publique située dans" \
+        " le 10e arrondissement de Paris.\n\n\n== Situation et accès" \
+        " ==\nLa cité Paradis est une voie publique située dans le 10e " \
+        "arrondissement de Paris. Elle est en forme de té, une branche" \
+        " débouche au 43, rue de Paradis, la deuxième au 57, rue " \
+        "d'Hauteville et la troisième en impasse."
         url = "https://fr.wikipedia.org/wiki/Cit%C3%A9_Paradis"
 
         class Page:
@@ -144,7 +172,12 @@ class TestAPI:
         mocker.patch("flaskapp.backend.API.MediaWiki.page.url", return_value=url)
 
         assert wiki.get_infos("Cité Paradis") == {
-            "summary": "La cité Paradis est une voie publique située dans le 10e arrondissement de Paris.\n\n\n\nLa cité Paradis est une voie publique située dans le 10e arrondissement de Paris. Elle est en forme de té, une branche débouche au 43, rue de Paradis, la deuxième au 57, rue d'Hauteville et la troisième en impasse.",
+            "summary": "La cité Paradis est une voie publique située" \
+            " dans le 10e arrondissement de Paris.\n\n\n\nLa cité Paradis" \
+            " est une voie publique située dans le 10e arrondissement de " \
+            "Paris. Elle est en forme de té, une branche débouche au 43, " \
+            "rue de Paradis, la deuxième au 57, rue d'Hauteville et la " \
+            "troisième en impasse.",
             "url": "https://fr.wikipedia.org/wiki/Cit%C3%A9_Paradis",
         }
 
@@ -153,7 +186,10 @@ class TestAPI:
 
         titles = []
 
-        mocker.patch("flaskapp.backend.API.MediaWiki.search", return_value=titles)
+        mocker.patch(
+                        "flaskapp.backend.API.MediaWiki.search", 
+                        return_value=titles
+                    )
 
         assert wiki.get_infos("whatever") == {
             "summary": "",
