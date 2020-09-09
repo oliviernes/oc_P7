d2b1049aa16c289aba3ed025b28505cb6eed29b4
Olivier71 <olivier.nesme@gmail.com>
2020-09-09 15:35:40 +0200

    Put messages tests in class TestMessages

--- test_grandpy_bot.py
+++ test_grandpy_bot.py
@@ -7,35 +7,37 @@ from flaskapp.backend.API import Google, WikiMedia, GetJson
 from flaskapp.backend.messages import Message
 
 
-def test_parser_adresse():
-    adresse = (
-        "Bonjour Grandpy, dis moi ce que tu sais à propos du 7 Cité"
-        " Paradis, 75010 Paris"
-    )
-    phrase_parsed = Parser()
+class TestParser:
 
-    assert phrase_parsed.parse(adresse) == "7 cite paradis 75010 paris"
+    def test_parser_adresse(self):
+        adresse = (
+            "Bonjour Grandpy, dis moi ce que tu sais à propos du 7 Cité"
+            " Paradis, 75010 Paris"
+        )
+        phrase_parsed = Parser()
 
-
-def test_parser_empty_string():
-    adresse = ""
-    phrase_parsed = Parser()
-
-    assert phrase_parsed.parse(adresse) == ""
+        assert phrase_parsed.parse(adresse) == "7 cite paradis 75010 paris"
 
 
-def test_parser_STOP_WORDS_only():
-    adresse = "a abord absolument afin ah ai aie ailleurs ainsi ait allaient"
-    phrase_parsed = Parser()
+    def test_parser_empty_string(self):
+        adresse = ""
+        phrase_parsed = Parser()
 
-    assert phrase_parsed.parse(adresse) == ""
+        assert phrase_parsed.parse(adresse) == ""
 
 
-def test_parser_apostrophe():
-    adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos" " d'Openclassrooms"
-    phrase_parsed = Parser()
+    def test_parser_STOP_WORDS_only(self):
+        adresse = "a abord absolument afin ah ai aie ailleurs ainsi ait allaient"
+        phrase_parsed = Parser()
 
-    assert phrase_parsed.parse(adresse) == "openclassrooms"
+        assert phrase_parsed.parse(adresse) == ""
+
+
+    def test_parser_apostrophe(self):
+        adresse = "Bonjour Grandpy, dis moi ce que tu sais à propos" " d'Openclassrooms"
+        phrase_parsed = Parser()
+
+        assert phrase_parsed.parse(adresse) == "openclassrooms"
 
 
 class TestAPI:
