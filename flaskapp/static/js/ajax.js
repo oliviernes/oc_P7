aea84a388bddd14ccea172c86590303bd28e7296
Olivier71 <olivier.nesme@gmail.com>
2020-09-10 10:11:45 +0200

    Display buildings in 3D with Mapbox

--- flaskapp/static/js/ajax.js
+++ flaskapp/static/js/ajax.js
@@ -2,6 +2,8 @@ const form = document.getElementById('formi');
 const spinner = document.getElementById('spin')
 let mapNumber = 0;
 
+// To switch to google map:
+
 // let map;
 
 // function initMap(mapa, lati, long) {
@@ -72,6 +74,60 @@ form.addEventListener('submit', function(event){
             var marker = new mapboxgl.Marker()
             .setLngLat([ long, lati ])
             .addTo(map);
+
+            // The 'building' layer in the mapbox-streets vector source contains building-height
+            // data from OpenStreetMap.
+            map.on('load', function () {
+                // Insert the layer beneath any symbol layer.
+                var layers = map.getStyle().layers;
+
+                var labelLayerId;
+                for (var i = 0; i < layers.length; i++) {
+                    if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+                    labelLayerId = layers[i].id;
+                    break;
+                    }
+                }
+
+                map.addLayer(
+                    {
+                        'id': '3d-buildings',
+                        'source': 'composite',
+                        'source-layer': 'building',
+                        'filter': ['==', 'extrude', 'true'],
+                        'type': 'fill-extrusion',
+                        'minzoom': 15,
+                        'paint': {
+                            'fill-extrusion-color': '#aaa',
+
+                            // use an 'interpolate' expression to add a smooth transition effect to the
+                            // buildings as the user zooms in
+                            'fill-extrusion-height': [
+                                'interpolate',
+                                ['linear'],
+                                ['zoom'],
+                                15,
+                                0,
+                                15.05,
+                                ['get', 'height']
+                            ],
+                            'fill-extrusion-base': [
+                                'interpolate',
+                                ['linear'],
+                                ['zoom'],
+                                15,
+                                0,
+                                15.05,
+                                ['get', 'min_height']
+                            ],
+                            'fill-extrusion-opacity': 0.6
+                        }
+                    },
+                    labelLayerId
+                );
+            });
+
+            // To init google map:
 
             // initMap(mapa, lati, long);
 
